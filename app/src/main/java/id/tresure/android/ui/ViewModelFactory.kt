9622436a29a6c4f546451de2c47e3e5854f5a865
rhnlf_ <alif.satoe@gmail.com>
2023-06-12 01:48:33 +0700

    [Add] DataStore and Helper - Helper for Invalid Email and Password - User Model and UserPreferences - ViewModelFactory

---
+++ app/src/main/java/id/tresure/android/ui/ViewModelFactory.kt
@@ -0,0 +1,35 @@
+package id.tresure.android.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import id.tresure.android.data.local.UserPreference
+import id.tresure.android.ui.home.HomeViewModel
+import id.tresure.android.ui.login.LoginViewModel
+import id.tresure.android.ui.register.RegisterViewModel
+
+class ViewModelFactory(
+    private val pref: UserPreference, private val application: Application
+) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                RegisterViewModel(application) as T
+            }
+
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(pref, application) as T
+            }
+
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+                HomeViewModel(application) as T
+            }
+
+
+            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+        }
+    }
+}
