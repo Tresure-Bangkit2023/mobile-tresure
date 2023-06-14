fa906e7a66bac898fbae407c5f8e3fd68f53fa38
rhnlf_ <alif.satoe@gmail.com>
2023-06-14 15:31:47 +0700

    [Add] Detail Place - Add Layout for Detail Place

---
+++ app/src/main/java/id/tresure/android/ui/detailplace/DetailPlaceActivity.kt
@@ -0,0 +1,12 @@
+package id.tresure.android.ui.detailplace
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.tresure.android.R
+
+class DetailPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_place)
+    }
+}
